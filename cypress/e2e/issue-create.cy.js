adb02951a97b668f696ee8b8c7fc1be15561e35a
Vladislavinho <anuvla11032003@mail.ru>
2023-07-04 20:54:23 +0300

    Commit my example.

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -32,8 +32,29 @@ describe('Issue create', () => {
       //Click on button "Create issue"
       cy.get('button[type="submit"]').click();
     })
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 5 issues and first element with tag p has specified text
+      cy.get('[data-testid="list-issue"]')
+        .should('have.length', '5')
+        .first()
+        .find('p')
+        .contains('AB');
+      //Assert that correct avatar and type icon are visible
+      cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
+      cy.get('[data-testid="icon:story"]').should('be.visible');
+
+    })
   })
-  it('Should create an issue and validate it successfully', () => {
+  it.only('Should create an issue and validate it successfully', () => {
     //System finds modal for creating issue and does next steps inside of it
     cy.get('[data-testid="modal:issue-create"]').within(() => {
 
