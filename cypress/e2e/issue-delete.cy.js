4f7f25e5298c9b07e890cc869b607191f50b3694
Vladislavinho <anuvla11032003@mail.ru>
2023-07-08 19:26:51 +0300

    Comiit my example.

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -7,15 +7,36 @@ cy.contains('This is an issue of type: Task.').click();
 });
 cy.get('[data-testid="modal:issue-details"]').should('be.visible')
 });
-const WindowDeleteissue = () => cy.get('[data-testid="modal:confirm"]');
+
 const OpenIssue = () => cy.get('[data-testid="modal:issue-details"]');
 it('Delete issue', () => {
  OpenIssue()
+ //Find and click on Delete button
  cy.get('[data-testid="icon:trash"]').click()
- cy.get(WindowDeleteissue).should('be.visible');
- cy.get(WindowDeleteissue).contains('Delete issue').click()
- cy.get(WindowDeleteissue).should('not.exist')
+ cy.get('[data-testid="modal:confirm"]').should('be.visible');
+ cy.get('[data-testid="modal:confirm"]').contains('Delete issue').click()
+ cy.get('[data-testid="modal:confirm"]').should('not.exist')
+ //Issue is not visible
+ cy.contains('This is an issue of type: Task.').should('not.exist')
 })
+it.only('Delete issue', () => {
+ OpenIssue()
+ //Try to delete issue
+ cy.get('[data-testid="icon:trash"]').click()
+ cy.get('[data-testid="modal:confirm"]').should('be.visible');
+ cy.get('[data-testid="modal:confirm"]').contains('Delete issue').click()
+ //Click on Cancel button
+ cy.get('[data-testid="modal:confirm"]').contains('Cancel').click()
+ cy.get('[data-testid="modal:confirm"]').should('not.exist')
+//Issue is visible
+ cy.contains('This is an issue of type: Task.').should('exist')
 
 
+
+
+
+
+
+
+})
 })
